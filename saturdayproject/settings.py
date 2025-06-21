fc0bef52b8c61c28786a9ce5ddd3ef1ab303b77c
BRV25 <atusinguza60@gmail.com>
2025-06-21 16:33:12 +0300

    second commit

--- saturdayproject/settings.py
+++ saturdayproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3wamb*4+$35sk3jc0mdofhnu!9!pz@6pqv$j_^-l%*9hh5a9h_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["x"]
 
 
 # Application definition
